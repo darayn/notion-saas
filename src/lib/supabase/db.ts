449ab7a573e753f3873ea7eb76168bda1ed6441d
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2024-04-07 16:22:08 +0530

    Drizzle flow setup pull

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,26 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotnev from 'dotenv';
+import * as schema from '../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+dotnev.config({path: '.env'})
+
+if (!process.env.DATABASE_URL){
+    console.log('⛔️ no database URL');
+}
+
+const client = postgres(process.env.DATABASE_URL as string , {max: 1});
+const db = drizzle(client, { schema })
+const migrateDb =async () => {
+    try {
+        console.log('🟠 Migrating Client');
+        await migrate(db, {migrationsFolder: "migrations"})
+        console.log(' 🟢 Successfully migrated');
+    } catch (error) {
+        console.log(error)
+        console.log('🔴 Error Migrating Client')
+    }
+};
+migrateDb();
+export default db;
